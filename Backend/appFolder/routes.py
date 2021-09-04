41fd1c6b718be4e936739450ffe40d4bac2f5b4c
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2021-09-03 22:52:36 -0700

    user profile delete update get end points

--- Backend/appFolder/routes.py
+++ Backend/appFolder/routes.py
@@ -5,6 +5,16 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from appFolder import app, db, api
 import json
 
+
+user_update_args = reqparse.RequestParser()
+user_update_args.add_argument('email', type=str, help='email')
+user_update_args.add_argument('firstName', type=str, help='first name')
+user_update_args.add_argument('secondName', type=str, help='second name')
+user_update_args.add_argument('password', type=str, help='password')
+user_update_args.add_argument('userType', type=str, help='user type')
+user_update_args.add_argument('photo', type=str, help='photo')
+
+
 resource_fields = {
     'user_id': fields.Integer,
     'email': fields.String,
@@ -47,5 +57,46 @@ class UserSignUp(Resource):
         return new_user
 
 
+class UserProfile(Resource):
+    @marshal_with(resource_fields)
+    def get(self, id):
+        result = User.query.filter_by(user_id=id).all()
+        if result:
+            return result
+        return "user id not found"
+
+    def delete(self, id):
+        result = User.query.filter_by(user_id=id).first()
+
+        if result:
+            db.session.delete(result)
+            db.session.commit()
+        return 'deleted successfully'
+
+    @marshal_with(resource_fields)
+    def patch(self, id):
+        args = user_update_args.parse_args()
+        result = User.query.filter_by(user_id=id).first()
+        if not result:
+            abort(404, message="id doesn't exist, cannot update")
+
+        if args['email']:
+            result.email = args['email']
+        if args['firstName']:
+            result.firstName = args['firstName']
+        if args['secondName']:
+            result.secondName = args['secondName']
+        if args['password']:
+            result.password = args['password']
+        if args['photo']:
+            result.photo = args['photo']
+        if args['userType']:
+            result.userType = args['userType']
+        db.session.commit()
+
+        return result
+
+
 api.add_resource(UseSignIn, "/api/v1/user/login")
 api.add_resource(UserSignUp,  "/api/v1/user/register")
+api.add_resource(UserProfile,  "/api/v1/user/profile/<int:id>")
