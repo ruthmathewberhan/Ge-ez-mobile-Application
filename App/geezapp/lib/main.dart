80bea39a9642e0dce4cfb124a9c508444416d8a2
Rebecca Samuel <rebeccasamuel999@gmail.com>
2021-09-10 02:33:13 +0300

    resolve merge conflict

--- App/geezapp/lib/main.dart
+++ App/geezapp/lib/main.dart
@@ -1,34 +1,80 @@
 // @dart=2.9
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:geezapp/Lesson/blocs/lesson_bloc.dart';
+import 'package:geezapp/Lesson/blocs/blocs.dart';
 import 'package:geezapp/Lesson/data_providers/lesson-data-provider.dart';
 import 'package:geezapp/Lesson/repository/lesson-repository.dart';
-import 'package:geezapp/Lesson/blocs/blocs.dart';
-import 'package:geezapp/User/screens/welcome_screen.dart';
+import 'package:geezapp/profile/signup/bloc/signup_bloc.dart';
+import 'package:geezapp/profile/signup/repository/signup.dart';
+import 'package:geezapp/profile/signup/screens/signup.dart';
+import 'package:geezapp/course/screens/home_screen.dart';
+import 'package:geezapp/login/repository/login.dart';
+import 'package:geezapp/profile/profile_screen/bloc/profile_screen_bloc.dart';
+import 'package:geezapp/profile/profile_screen/data_provider/profile_screen.dart';
+import 'package:geezapp/profile/profile_screen/repository/profile_screen.dart';
+import 'package:geezapp/profile/signup/signup.dart';
+import 'Auth/auth_bloc.dart';
+import 'Auth/auth_event.dart';
+import 'Auth/auth_state.dart';
+import 'login/screens/login.dart';
 import 'package:geezapp/routes.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'course/screens/UserHomePage.dart';
 
+class SimpleBlocDelegate extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object event) {
+    super.onEvent(bloc, event);
+    print(event);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    super.onError(bloc, error, stackTrace);
+    print(error);
+  }
+}
+
 void main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences preferences = await SharedPreferences.getInstance();
   var email = preferences.getString('email');
+  Bloc.observer = SimpleBlocDelegate();
 
-  final lessonRepository = LessonRepository(
-    LessonDataProvider(),
-  );
+  final userRepository = UserRepository();
+  final signupRepository = SignupRepository(dataProvider: SignupDataProvider());
+  final lessonRepository = LessonRepository(LessonDataProvider());
 
+  final profileScreenRepository =
+      ProfileScreenRepository(dataProvider: ProfileScreenDataProvider());
   runApp(MultiBlocProvider(
     providers: [
+      BlocProvider(create: (ctx) => LessonBloc(lessonRepository: lessonRepository))
+      BlocProvider<AuthenticationBloc>(
+        create: (context) {
+          return AuthenticationBloc(userRepository: userRepository)
+            ..add(AppStarted());
+        },
+      ),
       BlocProvider(
-        create: (ctx) => LessonBloc(lessonRepository: lessonRepository)
-      )
+          create: (ctx) => ProfileScreenBloc(
+              profileScreenRepository: profileScreenRepository)),
+       BlocProvider(
+          create: (ctx) => SignupBloc(
+              signupRepository: signupRepository)),   
     ],
-    child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        routes: AppRoute.routes,
-        initialRoute:
-            email == null ? AppRoute.initialRoute : UserHomePage.routeName),
-  ));
+    child: MaterialApp(routes:AppRoute.routes,
+    initialRoute: AppRoute.initialRoute, 
+    ),
+
+  ),
+
+  );
 }
