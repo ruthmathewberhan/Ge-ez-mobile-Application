bd1a4ab349436cd4dff0fa5c531779b3f86dd712
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2021-09-10 00:57:53 -0700

    update some logic

--- App/geezapp/lib/main.dart
+++ App/geezapp/lib/main.dart
@@ -1,25 +1,75 @@
 // @dart=2.9
 import 'package:flutter/material.dart';
-import 'package:geezapp/User/screens/login_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geezapp/profile/signup/bloc/signup_bloc.dart';
+import 'package:geezapp/profile/signup/repository/signup.dart';
+import 'package:geezapp/profile/signup/screens/signup.dart';
+import 'package:geezapp/course/screens/home_screen.dart';
+import 'package:geezapp/login/repository/login.dart';
+import 'package:geezapp/profile/profile_screen/bloc/profile_screen_bloc.dart';
+import 'package:geezapp/profile/profile_screen/data_provider/profile_screen.dart';
+import 'package:geezapp/profile/profile_screen/repository/profile_screen.dart';
+import 'package:geezapp/profile/signup/signup.dart';
+import 'Auth/auth_bloc.dart';
+import 'Auth/auth_event.dart';
+import 'Auth/auth_state.dart';
+import 'login/screens/login.dart';
 import 'package:geezapp/routes.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'course/screens/UserHomePage.dart';
+
+class SimpleBlocDelegate extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object event) {
+    super.onEvent(bloc, event);
+    print(event);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    super.onError(bloc, error, stackTrace);
+    print(error);
+  }
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences preferences = await SharedPreferences.getInstance();
   var email = preferences.getString('email');
-  runApp(MaterialApp(debugShowCheckedModeBanner: false,
-       home: email ==null? LoginScreen(): UserHomePage()));
+  Bloc.observer = SimpleBlocDelegate();
+
+  final userRepository = UserRepository();
+  final signupRepository = SignupRepository(dataProvider: SignupDataProvider());
+
+  final profileScreenRepository =
+      ProfileScreenRepository(dataProvider: ProfileScreenDataProvider());
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider<AuthenticationBloc>(
+        create: (context) {
+          return AuthenticationBloc(userRepository: userRepository)
+            ..add(AppStarted());
+        },
+      ),
+      BlocProvider(
+          create: (ctx) => ProfileScreenBloc(
+              profileScreenRepository: profileScreenRepository)),
+       BlocProvider(
+          create: (ctx) => SignupBloc(
+              signupRepository: signupRepository)),   
+    ],
+    child: MaterialApp(routes:AppRoute.routes,
+    initialRoute: AppRoute.initialRoute, 
+    ),
+
+  ),
+
+  );
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override 
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: AppRoute.routes,
-      initialRoute: AppRoute.initialRoute,
-    );
-  }
-}
--- App/geezapp/lib/routes.dart
+++ App/geezapp/lib/routes.dart
@@ -1,27 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:geezapp/User/screens/profile_screen.dart';
-import 'package:geezapp/User/screens/sign_up_screen.dart';
+import 'package:geezapp/profile/signup/screens/signup.dart';
 import 'package:geezapp/User/screens/welcome_screen.dart';
-import 'package:geezapp/User/screens/login_screen.dart';
+import 'package:geezapp/profile/profile_screen/screens/profile_screen.dart';
 import 'package:geezapp/course/screens/GrammarPage.dart';
 import 'package:geezapp/course/screens/UserHomePage.dart';
 import 'package:geezapp/course/screens/Courses2.dart';
 import 'package:geezapp/course/screens/home_screen.dart';
-
+import 'package:geezapp/login/screens/login.dart';
 import 'package:geezapp/User/screens/profile_edit.dart';
 
+import 'login/login_auth.dart';
+import 'login/repository/login.dart';
+
 class AppRoute {
-  static const initialRoute = LoginScreen.routeName;
+  static const initialRoute = LoginAuth.routeName;
 
   static final routes = {
     WelcomeScreen.routeName: (BuildContext context) => WelcomeScreen(),
-    LoginScreen.routeName: (BuildContext context) => LoginScreen(),
+    LoginScreen.routeName: (BuildContext context) => LoginScreen(userRepository: UserRepository(),),
     SignUpScreen.routeName: (BuildContext context) => SignUpScreen(),
     ProfileScreen.routeName: (BuildContext context) => ProfileScreen(),
     UserHomePage.routeName: (BuildContext context) => UserHomePage(),
     Courses2.routeName: (BuildContext context) => Courses2(),
     GrammarPage.routeName: (BuildContext context) => GrammarPage(),
     HomeScreen.routeName: (BuildContext context) => HomeScreen(),
-    ProfileEdit.routeName: (BuildContext context) => ProfileEdit()
+    ProfileEdit.routeName: (BuildContext context) => ProfileEdit(),
+    LoginAuth.routeName: (BuildContext context) => LoginAuth(userRepository: UserRepository(),),
+
   };
 }
